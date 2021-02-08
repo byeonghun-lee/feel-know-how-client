006f9303ad2e00d3c8a348613fbe66304802e78e
byeonghun <byeonghun08@gmail.com>
2021-02-08 21:28:50 +0900

    Best Main 라우터 설정

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import BestMain from "bestMain/BestMain";
 
 export default () => (
     <Switch>
+        <Route exact path="/">
+            <BestMain />
+        </Route>
         <Route path="/@:nickName">
             <h2>test</h2>
         </Route>
-        {/* <Route path="/in-box" />
+        <Route path="/in-box" />
         <Route path="/trash" />
-        <Route path="/setting" /> */}
+        <Route path="/setting" />
     </Switch>
 );
