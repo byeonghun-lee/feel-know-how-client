8e0d73edead4bcbb3bdfff08f4bfcba3c8ecdc0d
byeonghun <byeonghun08@gmail.com>
2021-03-21 21:36:32 +0900

    proxy 설정 변경(cors에러)

--- src/api/client.js
+++ src/api/client.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const client = axios.create();
 client.defaults.baseURL =
     process.env.NODE_ENV === "development"
-        ? "/"
+        ? "http://localhost:4000/"
         : "https://api.ohmydrawer.com/";
-client.defaults.withCredentials = "include";
+client.defaults.withCredentials = true;
 
 export default client;
