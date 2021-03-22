8f11544ddd777353f3e3342ea2e4dd52dfd0fd41
byeonghun <byeonghun08@gmail.com>
2021-03-22 21:38:17 +0900

    cors test

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,7 @@
 import client from "./client";
 
+console.log("client:", client.defaults);
 export const login = ({ id, password }) =>
-    client.post("/auth/login", { id, password });
+    client.post("/auth/login", { id, password }, { withCredentials: true });
 
 export const checkLogin = () => client.get("/auth/check");
