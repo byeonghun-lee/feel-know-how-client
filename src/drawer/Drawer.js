9de00afe30350f3a6e4403e836a3583a3b19068e
byeonghun <byeonghun08@gmail.com>
2021-04-08 22:54:43 +0900

    card page 기능 연결

--- src/drawer/Drawer.js
+++ src/drawer/Drawer.js
@@ -1,23 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
 import AddCardBtn from "components/addCardBtn/AddCardBtn";
 import CardComponent from "components/cardComponent/CardComponent";
+
+import { getList } from "service/card/cardSlice";
 
 import "./Drawer.scss";
 
 const Drawer = () => {
-    const { drawerName } = useParams();
+    const { drawerName, nickName } = useParams();
+    const dispatch = useDispatch();
+    const cardListPage = useSelector(({ card }) => card.list);
+
+    useEffect(() => {
+        dispatch(getList({ nickName, drawerName }));
+    }, [dispatch, drawerName]);
+
+    if (!cardListPage.drawerName) return null;
 
     return (
         <div className="drawer-page">
-            <h2>{drawerName}</h2>
-            <CardComponent />
-            <CardComponent />
-            <CardComponent />
-            <CardComponent />
-            <CardComponent />
-            <CardComponent />
-            <CardComponent />
+            <h2>{cardListPage.drawerName}</h2>
+            <p className="desc">{cardListPage.drawerDesc}</p>
+            <ul className="tag-list">
+                {cardListPage.tagList.map((tag, tagIndex) => (
+                    <li key={tagIndex}>{tag}</li>
+                ))}
+            </ul>
+            {cardListPage.cardList.map((cardInfo, index) => (
+                <CardComponent key={index} cardInfo={cardInfo} />
+            ))}
             <AddCardBtn />
         </div>
     );
