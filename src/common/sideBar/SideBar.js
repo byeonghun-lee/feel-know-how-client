f41eaa10044a2ebeff1291433fbc74a3929477b9
byeonghun <byeonghun08@gmail.com>
2021-05-16 21:34:33 +0900

    drawer페이지 card component 모바일 사이즈

--- src/common/sideBar/SideBar.js
+++ src/common/sideBar/SideBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import cx from "classnames";
 import { useSelector, useDispatch } from "react-redux";
 
