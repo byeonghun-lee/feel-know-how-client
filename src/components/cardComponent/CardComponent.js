c33485cd59795faa8470a3b583d94eea6ed5a6d3
byeonghun <byeonghun08@gmail.com>
2021-06-14 22:15:04 +0900

    카드에 기능 추가 위해 스타일 변경

--- src/components/cardComponent/CardComponent.js
+++ src/components/cardComponent/CardComponent.js
@@ -1,30 +1,72 @@
 import React from "react";
 
+import Switch from "@material-ui/core/Switch";
 import MoreVertRoundedIcon from "@material-ui/icons/MoreVertRounded";
 import LinkRoundedIcon from "@material-ui/icons/LinkRounded";
+import LaunchRoundedIcon from "@material-ui/icons/LaunchRounded";
+import { withStyles } from "@material-ui/core/styles";
 
 import "./CardComponent.scss";
 
+const PurpleSwitch = withStyles({
+    switchBase: {
+        color: "#bdbdbd",
+        "&$checked": {
+            color: "#512da8",
+        },
+        "&$checked + $track": {
+            backgroundColor: "#512da8",
+        },
+    },
+    checked: {},
+    track: {},
+})(Switch);
+
 const CardComponent = ({ cardInfo }) => {
+    const siteNameUrl = cardInfo.url.replace("https://", "").split("/")[0];
+
     return (
-        <a
-            className="card-component"
-            href={cardInfo.url}
-            target="_blank"
-            rel="noopener noreferrer"
-        >
+        <div className="card-component">
             <div className="corner-btn-area">
                 <MoreVertRoundedIcon className="corner-btn" />
             </div>
             <div className="text-contents">
-                <div className="title-area">
+                <a
+                    className="title-area"
+                    href={cardInfo.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <LinkRoundedIcon className="link-icon" />
                     <h3 className="card-component-title">{cardInfo.title}</h3>
-                </div>
+                </a>
                 <p className="card-component-desc">{cardInfo.desc}</p>
-                <p className="card-component-url">{cardInfo.url}</p>
+                <ul className="status-list">
+                    <li>
+                        <h4>Link</h4>
+                        <div className="site-url-tag">
+                            {siteNameUrl.split(".").length > 2
+                                ? siteNameUrl.split(".")[1]
+                                : siteNameUrl.split(".")[0]}
+                        </div>
+                        <a
+                            href={cardInfo.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="link-tag"
+                        >
+                            <LaunchRoundedIcon className="launch-icon" />
+                            link
+                        </a>
+                    </li>
+                    <li>
+                        <h4>Read Status</h4>
+                        <PurpleSwitch />
+                    </li>
+                </ul>
+                {/* <p className="card-component-url">{cardInfo.url}</p> */}
             </div>
-        </a>
+        </div>
     );
 };
 
