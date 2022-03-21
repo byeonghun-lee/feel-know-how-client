9eaa25ea5bdddda4079fa5c09e193b80df9b5ba2
byeonghun <byeonghun08@gmail.com>
2022-03-21 22:46:03 +0900

    빠진 api 추가

--- src/api/card.js
+++ src/api/card.js
@@ -1,7 +1,8 @@
 import client from "./client";
 
 export const createCard = (cardObj) => client.post("/cards", cardObj);
-export const getCard = ({ nickName, drawerName }) =>
-    client.get(`/cards?nickname=${nickName}&drawername=${drawerName}`);
+export const getCard = ({ nickname, drawerName }) =>
+    client.get(`/cards?nickname=${nickname}&drawername=${drawerName}`);
+export const getTempCard = ({ name }) => client.get(`/cards/temp?name=${name}`);
 export const updateCardReadStatus = (cardId) =>
     client.patch(`/cards/${cardId}/read-status`);
