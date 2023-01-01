43eae9fed621d08013b060c35e91478a317f5e97
byeonghun <byeonghun08@gmail.com>
2023-01-01 17:53:47 +0900

    카카오 광고 삽입

--- src/drawer/Drawer.js
+++ src/drawer/Drawer.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { isMobile } from "react-device-detect";
 
 import AddCardBtn from "components/addCardBtn/AddCardBtn";
 import CardComponent from "components/cardComponent/CardComponent";
@@ -19,12 +20,43 @@ const Drawer = () => {
     const dispatch = useDispatch();
     const user = useSelector(({ auth }) => auth.info);
     const cardListPage = useSelector(({ card }) => card.list);
+    const kakaoAdBox = useRef();
 
     const onToggleReadStatus = ({ cardId, e }) => {
         e.stopPropagation();
         if (!cardListPage.isOwner) return;
         dispatch(updateCardReadStatus(cardId));
     };
+
+    useEffect(() => {
+        const ins = document.createElement("ins");
+        ins.className = "kakao_ad_area";
+        ins.style = "display:none; width:100%;";
+        const adUnit = {
+            width: "160",
+            height: "600",
+            value: "DAN-Y2rrbLzkjMqOPefK",
+        };
+
+        if (isMobile) {
+            adUnit.width = "320";
+            adUnit.height = "50";
+            adUnit.value = "DAN-MUlLDLfUfKZtpv7Q";
+        }
+        ins.setAttribute("data-ad-width", adUnit.width);
+        ins.setAttribute("data-ad-height", adUnit.height);
+        ins.setAttribute("data-ad-unit", adUnit.value);
+
+        const scr = document.createElement("script");
+        scr.async = "true";
+        scr.type = "text/javascript";
+        scr.src = "//t1.daumcdn.net/kas/static/ba.min.js";
+
+        if (kakaoAdBox.current) {
+            kakaoAdBox.current.appendChild(ins);
+            kakaoAdBox.current.appendChild(scr);
+        }
+    }, [cardListPage.drawerName]);
 
     useEffect(() => {
         if (user && location.pathname === "/in-box") {
@@ -40,23 +72,28 @@ const Drawer = () => {
 
     return (
         <div className="drawer-page">
-            <div className="title-area">
-                <h2>{cardListPage.drawerName}</h2>
-                <p className="desc">{cardListPage.drawerDesc}</p>
-                <ul className="tag-list">
-                    {cardListPage.tagList.map((tag, tagIndex) => (
-                        <li key={tagIndex}>{tag}</li>
+            <div className="kakao-ad-box" ref={kakaoAdBox}></div>
+            <div className="drawer-contents">
+                <div className="title-area">
+                    <h2>{cardListPage.drawerName}</h2>
+                    <p className="desc">{cardListPage.drawerDesc}</p>
+                    <ul className="tag-list">
+                        {cardListPage.tagList.map((tag, tagIndex) => (
+                            <li key={tagIndex}>{tag}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="card-list">
+                    {cardListPage.cardList.map((cardInfo, index) => (
+                        <CardComponent
+                            key={index}
+                            cardInfo={cardInfo}
+                            isOwner={cardListPage.isOwner}
+                            onToggleReadStatus={onToggleReadStatus}
+                        />
                     ))}
-                </ul>
+                </div>
             </div>
-            {cardListPage.cardList.map((cardInfo, index) => (
-                <CardComponent
-                    key={index}
-                    cardInfo={cardInfo}
-                    isOwner={cardListPage.isOwner}
-                    onToggleReadStatus={onToggleReadStatus}
-                />
-            ))}
             {cardListPage.isOwner && <AddCardBtn />}
         </div>
     );
