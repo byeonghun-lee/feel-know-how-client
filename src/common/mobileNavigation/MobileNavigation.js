d245f52aa1115292b615dd1c8aca3d18c69e2041
byeonghun <byeonghun08@gmail.com>
2023-05-07 22:25:22 +0900

    모바일 네비게이션에서 현재 페이지 아이콘 활성화

--- src/common/mobileNavigation/MobileNavigation.js
+++ src/common/mobileNavigation/MobileNavigation.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import cx from "classnames";
 
 import StarRoundedIcon from "@material-ui/icons/StarRounded";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
@@ -10,24 +11,59 @@ import SettingsRoundedIcon from "@material-ui/icons/SettingsRounded";
 import "./MobileNavigation.scss";
 
 const MobileNavigation = () => {
+    const location = useLocation();
+
     return (
         <nav className="mobile-navigation">
-            <Link className="bar-item" to="/">
+            <Link
+                className={cx("bar-item", {
+                    active: location.pathname === "/",
+                })}
+                to="/"
+            >
                 <StarRoundedIcon />
                 <h2>Best</h2>
             </Link>
-            <Link className="bar-item" to="/drawers">
+            <Link
+                className={cx("bar-item", {
+                    active:
+                        location.pathname === "/drawers" ||
+                        location.search === "?return-page=drawers",
+                })}
+                to="/drawers"
+            >
                 <AllInboxIcon />
                 <h2>Drawers</h2>
             </Link>
-            <Link className="bar-item add-item" to="new-card">
+            <Link
+                className={cx("bar-item add-item", {
+                    active:
+                        location.pathname === "/new-card" ||
+                        location.search === "?return-page=new-card",
+                })}
+                to="/new-card"
+            >
                 <AddCircleIcon className="add-item-icon" />
             </Link>
-            <Link className="bar-item" to="/in-box">
+            <Link
+                className={cx("bar-item", {
+                    active:
+                        location.pathname === "/in-box" ||
+                        location.search === "?return-page=in-box",
+                })}
+                to="/in-box"
+            >
                 <InboxIcon />
                 <h2>Inbox</h2>
             </Link>
-            <Link className="bar-item" to="/setting">
+            <Link
+                className={cx("bar-item", {
+                    active:
+                        location.pathname === "/setting" ||
+                        location.search === "?return-page=setting",
+                })}
+                to="/setting"
+            >
                 <SettingsRoundedIcon />
                 <h2>Setting</h2>
             </Link>
