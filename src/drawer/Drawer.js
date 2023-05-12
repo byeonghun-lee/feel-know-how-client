c0f42d12edab06e05dd4f40c130b80d12c2aea69
byeonghun <byeonghun08@gmail.com>
2023-05-12 23:09:53 +0900

    카카오 광고 제거

--- src/drawer/Drawer.js
+++ src/drawer/Drawer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { isMobile } from "react-device-detect";
@@ -20,43 +20,12 @@ const Drawer = () => {
     const dispatch = useDispatch();
     const user = useSelector(({ auth }) => auth.info);
     const cardListPage = useSelector(({ card }) => card.list);
-    const kakaoAdBox = useRef();
 
     const onToggleReadStatus = ({ cardId, e }) => {
         e.stopPropagation();
         if (!cardListPage.isOwner) return;
         dispatch(updateCardReadStatus(cardId));
     };
-
-    useEffect(() => {
-        const ins = document.createElement("ins");
-        ins.className = "kakao_ad_area";
-        ins.style = "display:none; width:100%;";
-        const adUnit = {
-            width: "160",
-            height: "600",
-            value: "DAN-Y2rrbLzkjMqOPefK",
-        };
-
-        if (isMobile) {
-            adUnit.width = "320";
-            adUnit.height = "50";
-            adUnit.value = "DAN-MUlLDLfUfKZtpv7Q";
-        }
-        ins.setAttribute("data-ad-width", adUnit.width);
-        ins.setAttribute("data-ad-height", adUnit.height);
-        ins.setAttribute("data-ad-unit", adUnit.value);
-
-        const scr = document.createElement("script");
-        scr.async = "true";
-        scr.type = "text/javascript";
-        scr.src = "//t1.daumcdn.net/kas/static/ba.min.js";
-
-        if (kakaoAdBox.current) {
-            kakaoAdBox.current.appendChild(ins);
-            kakaoAdBox.current.appendChild(scr);
-        }
-    }, [cardListPage.drawerName]);
 
     useEffect(() => {
         if (user && location.pathname === "/in-box") {
@@ -72,7 +41,6 @@ const Drawer = () => {
 
     return (
         <div className="drawer-page">
-            <div className="kakao-ad-box" ref={kakaoAdBox}></div>
             <div className="drawer-contents">
                 <div className="title-area">
                     <h2>{cardListPage.drawerName}</h2>
